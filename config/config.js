01fa8451b50d1ff4ff360ae7e0035802407ff556
Johan Arroyo <jjoharr@gmail.com>
2020-01-06 08:57:22 -0600

    seguimiento

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const _ = require('lodash');
 
 const config = require('./config.json');
 const defaultConfig = config.development;
-const environment = process.env.NODE_ENV || 'development';
+const environment = process.env.NODE_ENV || 'production';
 const environmentConfig = config[environment];
 const finalConfig = _.merge(defaultConfig, environmentConfig);
 const fs = require('fs').promises
--- config/middlewares.js
+++ config/middlewares.js
@@ -8,11 +8,10 @@ process.env.NODE_ENV = 'development';
 const config = require('./config');
 app.use(logger('dev'));
 console.log("Aplicativo de la defensoria de los habitantes**");
-//cron.schedule('* * * * *', function() {
-console.log("running a job Consulta de Evidencias Externas**");
-config.log.info("***Inicia Consulta de Evidencias Externas***");
-evidenceService.getEvidence();
-//});
-
-
+//'23 17 * * *'
+cron.schedule('* * * * *', function() {
+    console.log("running a job Consulta de Evidencias Externas**");
+    config.log.info("***Inicia Consulta de Evidencias Externas***");
+    evidenceService.getEvidence();
+});
 module.exports = app;
